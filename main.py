727610a56857bd02906cf8c2a0ff9b7929ef5efa
Tadas <tadas.radz@gmail.com>
2021-01-31 11:08:19 +0200

    Added more columns

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from RPLCD import CharLCD
 
 GPIO.setmode(GPIO.BOARD)
 
-lcd = CharLCD(cols=12, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
+lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 
 
 def writeToLCD(text):

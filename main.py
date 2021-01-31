c62a0e8d199bf8b7e3d7030a995aa5e9d6665af3
Tadas <tadas.radz@gmail.com>
2021-01-31 11:04:42 +0200

    Updated to external

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from RPLCD import CharLCD
 
 GPIO.setmode(GPIO.BOARD)
 
-
 lcd = CharLCD(cols=12, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23], numbering_mode=GPIO.BOARD)
 
 
-def writeToLCD(lcd, text):
+def writeToLCD(text):
     lcd.write_string(text)
 
-writeToLCD(lcd ,'Labas Julija')
+
+writeToLCD('External variable')
